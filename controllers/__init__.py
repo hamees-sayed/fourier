b4abe740acd81b2aa8c93914e356fa193d1e6e71
Hamees Sayed <98336593+hamees-sayed@users.noreply.github.com>
2023-11-13 16:11:59 +0000

    removed yellow squigly lines

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -4,8 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-app = Flask(__name__, template_folder="../templates")
-app._static_folder = "../static"
+app = Flask(__name__, template_folder="../templates", static_folder = "../static")
 current_dir = os.path.abspath(os.path.dirname(__file__))
 project_root = os.path.abspath(os.path.join(current_dir, '..'))
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(project_root, 'database.sqlite3')}"
