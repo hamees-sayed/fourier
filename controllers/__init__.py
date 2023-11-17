9e23975b91c94fa99d7b7e69f4ad733931fd59e3
Hamees Sayed <98336593+hamees-sayed@users.noreply.github.com>
2023-11-17 16:44:50 +0000

    Pending changes exported from your codespace

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
@@ -11,6 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(project_root, 
 db = SQLAlchemy(app)
 app.app_context().push()
 bcrypt = Bcrypt(app)
+migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
 login_manager.login_message_category = "info"
