1aea1de12bc3be9a3fa7fdc646af418ac74c9dfd
Hamees Sayed <hameessayed71@gmail.com>
2024-04-16 12:49:35 +0000

    crontab timing

--- server/app.py
+++ server/app.py
@@ -5,15 +5,13 @@ from controllers.tasks import mail_users, mail_creators
 @celery.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(
-        #crontab(hour=19, minute=30),
-        10.0,
+        crontab(hour=19, minute=30),
         mail_users.s(), 
         name="Sends email everyday at 7:30 PM."
     )
     
     sender.add_periodic_task(
-        # crontab(0, 0, day_of_month='1'),
-        10.0,
+        crontab(0, 0, day_of_month='1'),
         mail_creators.s(), 
         name="Sends Report on the first day of every month."
     )
